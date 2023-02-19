0139bd041157d0a194e10a1642f0a09fedb06dc1
imrantahir16 <imrantahir16@gmail.com>
2023-02-20 00:53:21 +0500

    Database connection and cors origin setup

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/db");
+const mongoose = require("mongoose");
+
+require("colors");
+require("dotenv").config();
+
+const app = express();
+connectDB();
+// middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connection.once("open", () => {
+  console.log("Database connected".green.underline);
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`.cyan.underline);
+  });
+});
