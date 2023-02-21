2945e271f595806cc53332ee6dda2d208a8db99d
imrantahir16 <imrantahir16@gmail.com>
2023-02-22 01:50:32 +0500

    cleanup src directory and added home page

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,21 @@
+import styles from "./home.module.css";
+import image from "../../assets/89367.jpg";
+const Home = () => {
+  return (
+    <div>
+      <section className={styles.hero}>
+        <h1 className={styles.heading}>Easiest invoicing for businesses</h1>
+        <div className={styles.paragraph}>
+          <p>
+            Free and Open Source Invoicing application made with MongoDB,
+            Express, React & Nodejs
+          </p>
+        </div>
+        <div className={styles.imgContainer}>
+          <img src={image} alt="invoicing app mockup" />
+        </div>
+      </section>
+    </div>
+  );
+};
+export default Home;
